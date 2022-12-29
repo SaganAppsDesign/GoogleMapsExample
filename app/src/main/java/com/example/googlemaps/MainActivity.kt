9641375fd718e72413f71da68163325cae59cc69
david <davidaren@gmail.com>
2022-12-29 09:50:25 +0100

    ---

---
+++ app/src/main/java/com/example/googlemaps/MainActivity.kt
@@ -0,0 +1,114 @@
+package com.example.googlemaps
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.data.geojson.GeoJsonLayer
+
+internal class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val cadiz = LatLng(36.517676, -6.276978)
+        mMap.addMarker(MarkerOptions()
+            .position(cadiz)
+            .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cadiz))
+
+        enableMyLocation()
+
+        val carrilBici = GeoJsonLayer(mMap, R.raw.carril_bici, this)
+        val styleCarril = carrilBici.defaultLineStringStyle
+        styleCarril.color = Color.GREEN
+
+        val aparcabicis = GeoJsonLayer(mMap, R.raw.aparcabicis, this)
+        val styleAparcabicis = aparcabicis.defaultPointStyle
+
+        for (feature in aparcabicis.features) {
+            Log.i("feature", "$feature")
+            styleAparcabicis.title = feature.getProperty("name")
+        }
+        carrilBici.addLayerToMap()
+        aparcabicis.addLayerToMap()
+    }
+
+    private fun isPermissionsGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (!::mMap.isInitialized) return
+        if (isPermissionsGranted()) {
+            mMap.isMyLocationEnabled = true
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE_LOCATION = 0
+    }
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Toast.makeText(this, "Ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_LOCATION)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_CODE_LOCATION -> if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                mMap.isMyLocationEnabled = true
+            }else{
+                Toast.makeText(this, "Para activar la localización ve a ajustes y acepta los permisos", Toast.LENGTH_SHORT).show()
+            }
+            else -> {}
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "Boton pulsado", Toast.LENGTH_SHORT).show()
+        return false
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "Estás en ${p0.latitude}, ${p0.longitude}", Toast.LENGTH_SHORT).show()
+    }
+
+}
